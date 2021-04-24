c4f248f872c984da97ea146b018fee0db307f934
Vladimir <wowchik.shadrin@yandex.ru>
2021-04-25 01:06:05 +0300

    homework18

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+
+  function countTimer(deadline) {
+    let timerHours = document.querySelector("#timer-hours"),
+      timerMinutes = document.querySelector("#timer-minutes"),
+      timerSeconds = document.querySelector("#timer-seconds");
+
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining =
+          dateStop - dateNow > 0 ? (dateStop - dateNow) / 1000 : 0,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor((timeRemaining / 60 / 60) % 24);
+      //console.log("timeRemaining: ", timeRemaining);
+      return { timeRemaining, hours, minutes, seconds };
+
+      //days = Math.floor(timeRemaining / 60 / 60 / 24);
+    }
+
+    function withZero(digit) {
+      return digit < 10 ? "0" + digit : digit;
+    }
+    function updateClock() {
+      let timer = getTimeRemaining();
+      timerHours.textContent = withZero(timer.hours);
+      timerMinutes.textContent = withZero(timer.minutes);
+      timerSeconds.textContent = withZero(timer.seconds);
+    }
+    updateClock();
+    setInterval(updateClock, 1000);
+  }
+
+  countTimer("30 july 2021");
+});
