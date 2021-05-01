46e2ef1452f35618a0ffbbffa9ef8cf547075513
Vladimir <wowchik.shadrin@yandex.ru>
2021-05-01 19:21:15 +0300

    homework19 fixing

--- scripts/script.js
+++ scripts/script.js
@@ -60,4 +60,43 @@ window.addEventListener("DOMContentLoaded", function () {
     );
   };
   toggleMenu();
+
+  // popup window
+  const togglePopUp = () => {
+    const popUp = document.querySelector(".popup"),
+      body = document.querySelector("body"),
+      popupContent = document.querySelector(".popup-content");
+    (popUpBtn = document.querySelectorAll(".popup-btn")),
+      (popUpClose = document.querySelector(".popup-close"));
+    let animeInterval, count;
+    const popUpAnimate = () => {
+      animeInterval = requestAnimationFrame(popUpAnimate);
+      count++;
+
+      if (count < 38) {
+        popupContent.style.left = count + "%";
+        //console.log(" mainForm.style.left: ", popupContent.style.left);
+      } else {
+        cancelAnimationFrame(animeInterval);
+      }
+      //console.log("count: ", count);
+    };
+    popUpBtn.forEach((elem) => {
+      elem.addEventListener("click", () => {
+        popUp.style.display = "block";
+        count = -50;
+        //console.log("window.getComputedStyle(body).width: ", body.offsetWidth);
+        if (body.offsetWidth >= 768) {
+          animeInterval = requestAnimationFrame(popUpAnimate);
+        }
+        //popupContent.style.left = 0;
+      });
+    });
+    popUpClose.addEventListener("click", () => {
+      popUp.style.display = "none";
+    });
+  };
+  togglePopUp();
+
+  //tabs
 });
