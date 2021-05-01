2f2d9aabd6c7ecc75bbfe397ab4413d32944e730
Vladimir <wowchik.shadrin@yandex.ru>
2021-05-01 21:18:27 +0300

    homework20

--- scripts/script.js
+++ scripts/script.js
@@ -37,27 +37,33 @@ window.addEventListener("DOMContentLoaded", function () {
   // menu
   const toggleMenu = () => {
     const btnMenu = document.querySelector(".menu"),
-      menu = document.querySelector("menu"),
-      closeBtn = document.querySelector(".close-btn"),
-      menuItems = menu.querySelectorAll("ul>li");
+      menu = document.querySelector("menu");
+    //0closeBtn = document.querySelector(".close-btn"),
+    //menuItems = menu.querySelectorAll("ul>li");
 
     const handlerMenu = () => {
-      // if (
-      //   !menu.style.transform ||
-      //   menu.style.transform === `translate(-100%)`
-      // ) {
-      //   menu.style.transform = `translate(0)`;
-      // } else {
-      //   menu.style.transform = `translate(-100%)`;
-      // }
       menu.classList.toggle("active-menu");
     };
-    btnMenu.addEventListener("click", handlerMenu);
-    closeBtn.addEventListener("click", handlerMenu);
 
-    menuItems.forEach((element) =>
-      element.addEventListener("click", handlerMenu)
-    );
+    btnMenu.addEventListener("click", handlerMenu);
+
+    menu.addEventListener("click", (event) => {
+      let target = event.target;
+
+      if (target.classList.contains("close-btn")) {
+        handlerMenu();
+      } else {
+        target = target.closest("li");
+        if (target) {
+          handlerMenu();
+        }
+      }
+    });
+    // closeBtn.addEventListener("click", handlerMenu);
+
+    // menuItems.forEach((element) =>
+    //   element.addEventListener("click", handlerMenu)
+    // );
   };
   toggleMenu();
 
@@ -65,9 +71,9 @@ window.addEventListener("DOMContentLoaded", function () {
   const togglePopUp = () => {
     const popUp = document.querySelector(".popup"),
       body = document.querySelector("body"),
-      popupContent = document.querySelector(".popup-content");
-    (popUpBtn = document.querySelectorAll(".popup-btn")),
-      (popUpClose = document.querySelector(".popup-close"));
+      popupContent = document.querySelector(".popup-content"),
+      popUpBtn = document.querySelectorAll(".popup-btn");
+    //popUpClose = document.querySelector(".popup-close");
     let animeInterval, count;
     const popUpAnimate = () => {
       animeInterval = requestAnimationFrame(popUpAnimate);
@@ -92,11 +98,66 @@ window.addEventListener("DOMContentLoaded", function () {
         //popupContent.style.left = 0;
       });
     });
-    popUpClose.addEventListener("click", () => {
-      popUp.style.display = "none";
+    // popUpClose.addEventListener("click", () => {
+    //   popUp.style.display = "none";
+    // });
+
+    popUp.addEventListener("click", (event) => {
+      let target = event.target;
+
+      if (target.classList.contains("popup-close")) {
+        popUp.style.display = "none";
+      } else {
+        target = target.closest(".popup-content");
+        if (!target) {
+          popUp.style.display = "none";
+        }
+      }
     });
   };
   togglePopUp();
 
   //tabs
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header"),
+      tab = document.querySelectorAll(".service-header-tab"),
+      tabContent = document.querySelectorAll(".service-tab");
+    // console.log("tabContent: ", tabContent);
+    // console.log("tabHeader: ", tabHeader);
+    // console.log("tab: ", tab);
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
+
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+      console.log("target: ", target);
+      target = target.closest(".service-header-tab");
+      console.log("target: ", target);
+
+      //while (target !== tabHeader) {
+      //console.log("target: ", target);
+
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+          //    });
+          //    return;
+        });
+        //  target = target.parentNode;
+      }
+    });
+  };
+
+  tabs();
 });
