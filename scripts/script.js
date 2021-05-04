d587b39bc687eccd7d03eb01803984bbdfc95028
Vladimir <wowchik.shadrin@yandex.ru>
2021-05-04 20:04:43 +0300

    homework24

--- scripts/script.js
+++ scripts/script.js
@@ -290,9 +290,59 @@ window.addEventListener("DOMContentLoaded", function () {
   command();
 
   //calculator
-  const calculator = () => {
-    const summInput = document.querySelectorAll(".calc-item");
-    console.log("nameInput: ", summInput);
+  const calculator = (price = 100) => {
+    const calcBlock = document.querySelector(".calc-block"),
+      calcType = document.querySelector(".calc-type"),
+      calcSquare = document.querySelector(".calc-square"),
+      calcDay = document.querySelector(".calc-day"),
+      calcCount = document.querySelector(".calc-count"),
+      totalValue = document.getElementById("total"),
+      summInput = document.querySelectorAll(".calc-item");
+
+    console.log("totalValue: ", totalValue.textContent);
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+      //console.log("typeValue: ", typeValue);
+      //console.log("squareValue: ", squareValue);
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      } else {
+        total = 0;
+      }
+
+      totalValue.textContent = total;
+    };
+    calcBlock.addEventListener("change", (event) => {
+      const target = event.target;
+      // if (
+      //   target.matches(".calc-type") ||
+      //   target.matches(".calc-square") ||
+      //   target.matches(".calc-day") ||
+      //   target.matches(".calc-count")
+      // ) {
+      //   console.log(1);
+      // }
+      //
+
+      if (target.matches("select") || target.matches("input")) {
+        countSum();
+      }
+    });
+    //console.log("nameInput: ", summInput);
 
     const readInputSumm = (event) => {
       //console.log("event: ", event);
@@ -304,7 +354,7 @@ window.addEventListener("DOMContentLoaded", function () {
       item.addEventListener("keyup", readInputSumm);
     });
   };
-  calculator();
+  calculator(100);
 
   //connect
   const connect = () => {
